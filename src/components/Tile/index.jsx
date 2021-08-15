bd438798b922b7271ac8ef84985a96f3d3883cca
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 21:47:27 -0300

    feat: change color based on black prop

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,7 +1,14 @@
 import styles from './Tile.module.css'
 
 const Tile = (props) => {
-	return <div className={styles.tile} />
+	return (
+		<div
+			className={styles.tile}
+			style={{
+				backgroundColor: props.black ? '#000' : '#fff'
+			}}
+		/>
+	)
 }
 
 export default Tile
